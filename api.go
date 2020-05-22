9a7fb9bf4a6affa7cbe14d866e0964d4517ffaaa
John McKenzie <jmckind@gmail.com>
2020-05-22 14:59:31 -0500

    Refactor to use a handler object

--- api.go
+++ api.go
@@ -19,6 +19,7 @@ import (
 	"log"
 	"net/http"
 	"os/exec"
+	"strings"
 	"time"
 )
 
@@ -36,32 +37,65 @@ type Widget struct {
 	Description string `json:"description"`
 }
 
-var _widgets = make(map[string]Widget, 0)
+// WidgetHandler handles Widget requests.
+type WidgetHandler struct {
+	widgets map[string]Widget
+}
+
+// NewWidgetHandler will construct a new WidgetHandler.
+func NewWidgetHandler() WidgetHandler {
+	return WidgetHandler{
+		widgets: make(map[string]Widget, 0),
+	}
+}
+
+func (h WidgetHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	path := r.URL.EscapedPath()
+	id := strings.Replace(path, "/widgets/", "", 1)
+	log.Printf("path: %s method: %s id: %s", path, r.Method, id)
+
+	switch r.Method {
+	case http.MethodGet:
+		if len(id) > 0 {
+			h.get(w, r, id)
+		} else {
+			h.list(w, r)
+		}
+		return
+	case http.MethodPost:
+		if len(id) > 0 {
+			break
+		}
+		h.create(w, r)
+		return
+	case http.MethodPut:
+		if len(id) <= 0 {
+			break
+		}
+		h.update(w, r, id)
+		return
+	case http.MethodDelete:
+		if len(id) <= 0 {
+			break
+		}
+		h.delete(w, r, id)
+		return
+	default:
+		// default method not allowed...
+	}
+
+	writeJSONError(w, http.StatusMethodNotAllowed, "Method not allowed for this resource.")
+}
 
 func main() {
-	http.HandleFunc("/", handleIndex)
-	http.HandleFunc("/widgets/", handleWidgets)
+	http.HandleFunc("/", index)
+	http.Handle("/widgets/", NewWidgetHandler())
 
 	log.Printf("listening for connections at %s", listenAddress)
 	log.Fatal(http.ListenAndServe(listenAddress, nil))
 }
 
-func handleWidgets(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case http.MethodGet:
-		listWidgets(w, r)
-	case http.MethodPost:
-		createWidget(w, r)
-	case http.MethodPut:
-		updateWidget(w, r)
-	case http.MethodDelete:
-		deleteWidget(w, r)
-	default:
-		// Give an error message.
-	}
-}
-
-func handleIndex(w http.ResponseWriter, r *http.Request) {
+func index(w http.ResponseWriter, r *http.Request) {
 	log.Printf("URL Path: %s Method: %s", r.URL.Path, r.Method)
 	if r.URL.Path != "/" {
 		writeJSONError(w, http.StatusNotFound, "The requested resource could not be located.")
@@ -83,9 +117,9 @@ func handleIndex(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func listWidgets(w http.ResponseWriter, r *http.Request) {
+func (h WidgetHandler) list(w http.ResponseWriter, r *http.Request) {
 	widgets := make([]Widget, 0)
-	for _, widget := range _widgets {
+	for _, widget := range h.widgets {
 		widgets = append(widgets, widget)
 	}
 
@@ -99,9 +133,8 @@ func listWidgets(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func getWidget(w http.ResponseWriter, r *http.Request) {
-	id := "foo"
-	widget, ok := _widgets[id]
+func (h WidgetHandler) get(w http.ResponseWriter, r *http.Request, id string) {
+	widget, ok := h.widgets[id]
 	if !ok {
 		log.Printf("unable to find widget with id %s", id)
 		writeJSONError(w, http.StatusNotFound, "The requested resource could not be located.")
@@ -113,7 +146,7 @@ func getWidget(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func createWidget(w http.ResponseWriter, r *http.Request) {
+func (h WidgetHandler) create(w http.ResponseWriter, r *http.Request) {
 	var widget Widget
 
 	decoder := json.NewDecoder(r.Body)
@@ -129,17 +162,16 @@ func createWidget(w http.ResponseWriter, r *http.Request) {
 		writeJSONError(w, http.StatusInternalServerError, err.Error())
 		return
 	}
-	widget.ID = string(uuid)
-	_widgets[widget.ID] = widget
+	widget.ID = strings.TrimSpace(string(uuid))
+	h.widgets[widget.ID] = widget
 
 	if err := writeJSON(w, http.StatusCreated, map[string]Widget{"widget": widget}); err != nil {
 		writeJSONError(w, http.StatusInternalServerError, err.Error())
 	}
 }
 
-func updateWidget(w http.ResponseWriter, r *http.Request) {
-	id := "foo"
-	widget, ok := _widgets[id]
+func (h WidgetHandler) update(w http.ResponseWriter, r *http.Request, id string) {
+	widget, ok := h.widgets[id]
 	if !ok {
 		log.Printf("unable to find widget with id %s", id)
 		writeJSONError(w, http.StatusNotFound, "The requested resource could not be located.")
@@ -156,22 +188,21 @@ func updateWidget(w http.ResponseWriter, r *http.Request) {
 
 	widget.Name = updWidget.Name
 	widget.Description = updWidget.Description
-	_widgets[widget.ID] = widget
+	h.widgets[widget.ID] = widget
 
 	if err := writeJSON(w, http.StatusOK, map[string]Widget{"widget": widget}); err != nil {
 		writeJSONError(w, http.StatusInternalServerError, err.Error())
 	}
 }
 
-func deleteWidget(w http.ResponseWriter, r *http.Request) {
-	id := "foo"
-	widget, ok := _widgets[id]
+func (h WidgetHandler) delete(w http.ResponseWriter, r *http.Request, id string) {
+	widget, ok := h.widgets[id]
 	if !ok {
 		log.Printf("unable to find widget with id %s", id)
 		writeJSONError(w, http.StatusNotFound, "The requested resource could not be located.")
 		return
 	}
-	delete(_widgets, id)
+	delete(h.widgets, id)
 
 	if err := writeJSON(w, http.StatusOK, map[string]Widget{"widget": widget}); err != nil {
 		writeJSONError(w, http.StatusInternalServerError, err.Error())
